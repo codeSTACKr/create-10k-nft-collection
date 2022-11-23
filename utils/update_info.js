c77336a007caf9acb9671499cab9b0c5e192d225
codeSTACKr <codestackr@gmail.com>
2022-11-23 12:56:14 -0600

    update async function

--- utils/update_info.js
+++ utils/update_info.js
@@ -8,31 +8,35 @@ const {
   namePrefix,
 } = require(`${basePath}/src/config.js`);
 
-// read json data
-let rawdata = fs.readFileSync(`${basePath}/build/json/_metadata.json`);
-let data = JSON.parse(rawdata);
+(async () => {
 
-console.log("Info will be updated using the config.js data.");
-const updateName = await yesno(`Update names?`);
-const updateDescription = await yesno(`Update descriptions?`);
-const updateBaseUri = await yesno(`Update images base URI?`);
+  // read json data
+  let rawdata = fs.readFileSync(`${basePath}/build/json/_metadata.json`);
+  let data = JSON.parse(rawdata);
 
-data.forEach((item) => {
-  if(updateName) item.name = `${namePrefix} #${item.edition}`;
-  if(updateDescription) item.description = description;
-  if(updateBaseUri) item.image = `${baseUri}/${item.edition}.png`;
- 
+  console.log("Info will be updated using the config.js data.");
+  const updateName = await yesno(`Update names?`);
+  const updateDescription = await yesno(`Update descriptions?`);
+  const updateBaseUri = await yesno(`Update images base URI?`);
+
+  data.forEach((item) => {
+    if(updateName) item.name = `${namePrefix} #${item.edition}`;
+    if(updateDescription) item.description = description;
+    if(updateBaseUri) item.image = `${baseUri}/${item.edition}.png`;
+  
+    fs.writeFileSync(
+      `${basePath}/build/json/${item.edition}.json`,
+      JSON.stringify(item, null, 2)
+    );
+  });
+
   fs.writeFileSync(
-    `${basePath}/build/json/${item.edition}.json`,
-    JSON.stringify(item, null, 2)
+    `${basePath}/build/json/_metadata.json`,
+    JSON.stringify(data, null, 2)
   );
-});
 
-fs.writeFileSync(
-  `${basePath}/build/json/_metadata.json`,
-  JSON.stringify(data, null, 2)
-);
+  if(updateName) console.log(`Updated name prefix for images to ===> ${namePrefix}`);
+  if(updateBaseUri) console.log(`Updated baseUri for images to ===> ${baseUri}`);
+  if(updateDescription) console.log(`Updated description for images to ===> ${description}`);
 
-if(updateName) console.log(`Updated name prefix for images to ===> ${namePrefix}`);
-if(updateBaseUri) console.log(`Updated baseUri for images to ===> ${baseUri}`);
-if(updateDescription) console.log(`Updated description for images to ===> ${description}`);
+})();
